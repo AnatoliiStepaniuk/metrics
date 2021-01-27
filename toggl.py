e60768b97d1ef1732d20efde343fe0c20bbd104a
Anatolii Stepaniuk <astepaniuk@tektelic.com>
2021-01-27 19:25:50 +0200

    Refactoring (separate count of programming)

--- toggl.py
+++ toggl.py
@@ -19,7 +19,19 @@ def get_hours():
     until = arrow.now().format('YYYY-MM-DD')
     url = f'https://api.track.toggl.com/reports/api/v2/summary?user_agent={email_safe}&workspace_id={workspace_id}&since={since}&until={until}'
 
-    year_2020 = 2065
+    programming_year_2020 = 2065
     resp = requests.get(url, auth=(api_token, 'api_token'))
-    current_year = round(resp.json()['total_grand']/HOUR_IN_MILLIS)
-    return year_2020 + current_year
+    data = resp.json()['data']
+    programming_current_year = round((get_learning(data) + get_job(data))/HOUR_IN_MILLIS)
+    programming_total = programming_year_2020 + programming_current_year
+
+    return programming_total
+
+
+def get_learning(data):
+    return list(filter(lambda x: x['title']['project'] == 'Learning Programming', data))[0]['time']
+
+
+def get_job(data):
+    return list(filter(lambda x: x['title']['project'] == 'Tektelic', data))[0]['time']
+
